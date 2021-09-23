4a260ed747bef28ebd7992365f0bc3e0c76b1a8f
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-09-23 18:40:35 -0400

    add test for the code

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,11 +4,14 @@ const fetchBreedDescription = function(breedName, callback) {
   const API = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
   request(API, (error, response, body) => {
     if (error) callback(error, null); // Print the error if one occurred
-    
+
     const data = JSON.parse(body);
-    if (data.length === 0) console.log('Wrong breed name, try again! \n');
-    let description = data[0].description;
-    callback(null, description);
+
+    if (data.length === 0) callback(null, null);
+    else {
+      let description = data[0].description;
+      callback(null, description);
+    }
   });
 };
 
