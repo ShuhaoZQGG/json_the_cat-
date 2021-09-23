4dc75ac65f5d61da27373c3aa3d116c73ddf47ea
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-09-23 18:11:18 -0400

    refactor the code by using async callback

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,19 @@
 const request = require('request');
 
-let args = process.argv;
-let breed = args.slice(2)[0];
+const fetchBreedDescription = function(breedName, callback) {
+  const API = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(API, (error, response, body) => {
+    if (error) callback(error, null); // Print the error if one occurred
+    
+    const data = JSON.parse(body);
+    if (data.length === 0) console.log('Wrong breed name, try again! \n');
+    let description = data[0].description;
+    callback(null, description);
+  });
+};
 
-const API = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
-request(API, (error, response, body) => {
-  if (error) console.log(`Hey here is an Error:\n ${error.message} \n`); // Print the error if one occurred
-  
-  const data = JSON.parse(body);
-  if (data.length === 0) console.log('Wrong breed name, try again! \n');
-  let description = data[0].description;
-  console.log(description);
-});
+
+module.exports = {fetchBreedDescription}
 
 
